217313e774bc79ee8723a9cab4ecf5abb021b6b9
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-19 18:14:40 +0100

    Modelo de Usuario

--- todor/models.py
+++ todor/models.py
@@ -1,2 +1,21 @@
 ##Este fichero sera nuestro fichero principal
 
+from todor import db
+
+
+# Crea clase para usuarios
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True),
+    username = db.Column(db.String(20), unique=True, nullable=False),
+    password = db.Column(db.Text, nullable=False)
+
+
+# constructor
+
+def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+
+def __repr__(self):
+    return f"<User: {self.username}>"
